dcaff73ca4d92d9ada622f488e2f4cdda0ed0e3e
李鑫(媒体内容业务线.202368) <micro-tech@foxmail.com>
2014-01-21 17:18:30 +0800

    finish board function.

--- public/javascripts/board.js
+++ public/javascripts/board.js
@@ -1,11 +1,39 @@
-angular.element(document).ready(function () {
-  var channel = new Channel({
-    topics: "score",
-    token: "1ae7251784b3a77167795ca08a7fcd2f",
-    timestamp: "1390291727884",
-    appid: "1da52hlqpj"
+angular.module('app', ['ngResource'])
+  .controller('BoardCtrl', function ($scope, $resource) {
+
+
+    var Score = $resource('/api/score');
+    Score.query(function (list) {
+      $scope.list = list.sort(function (a, b) {
+        return b.score - a.score;
+      });
+    });
+
+    angular.element(document).ready(function () {
+      var channel = new Channel({
+        topics: "score",
+        token: "1ae7251784b3a77167795ca08a7fcd2f",
+        timestamp: "1390291727884",
+        appid: "1da52hlqpj"
+      });
+      channel.onmessage = function (msg) {
+
+        var player = JSON.parse(msg.data);
+        // force a $digest
+        $scope.$apply(function () {
+          for (var i = 0; i < $scope.list.length; i++) {
+            if ($scope.list[i].name == player.name) {
+              $scope.list[i].score = player.score;
+              break
+            }
+          };
+          $scope.list = $scope.list.sort(function (a, b) {
+            return b.score - a.score;
+          })
+        })
+
+      }
+    })
+
+
   });
-  channel.onmessage = function (msg) {
-    console.log(msg.topic, msg.data);
-  }
-})
